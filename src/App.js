b1e8b4fab35f7f52bda4d6243c58b71dedc5df98
Jerome Simms <jerome.simms@outlook.com>
2023-08-22 17:19:42 -0400

    Createthe Budget, Remaining, and ExpenseTotal components

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
+import ExpenseTotal from './components/ExpenseTotal';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -15,11 +16,20 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                        {/* Add Budget component here under */}
+                        <div className='col-sm'>
+                            <Budget />    
+                        </div>     
 
-                        {/* Add Remaining component here under */}        
+                        {/* Add Remaining component here under */} 
+                        <div className='col-sm'>
+                            <Remaining />    
+                        </div>         
 
-                        {/* Add ExpenseTotal component here under */}        
+                        {/* Add ExpenseTotal component here under */}
+                        <div className='col-sm'>
+                            <ExpenseTotal />    
+                        </div>         
                        
                         {/* Add ExpenseList component here under */}         
 
